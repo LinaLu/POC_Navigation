a713c3234847e6c460c203fb0c761624e3e8073a
Lina Lu <lina.my.lu@gmail.com>
2020-11-15 10:46:44 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Text, View } from "react-native";
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Poc using react native web</Text>
+    </View>
+  );
+};
+
+export default App;
